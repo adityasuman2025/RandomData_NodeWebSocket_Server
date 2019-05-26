d13fbb8dbb8ada033d75688b45bb8fd8fc366150
adityasuman2025 <aditya.me16@iitp.ac.in>
2019-05-26 11:49:52 +0530

    minor change

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ wss.on('connection', function connection(ws)
     var randomNum1 = Math.floor(Math.random() * 10000);  
     var randomNum2 = Math.floor(Math.random() * 1000);  
     var randomNum3 = Math.floor(Math.random() * 100);  
-    var randomNum4 = Math.floor(Math.random() * 11);  
-    var randomNum5 = Math.floor(Math.random() * 10);  
-    var randomNum6 = Math.floor(Math.random() * 5);  
+    var randomNum4 = Math.floor(Math.random() * 50);  
+    var randomNum5 = Math.floor(Math.random() * 20);  
+    var randomNum6 = Math.floor(Math.random() * 10);  
 
     var jdata = {"EQUIP_STATUS": randomNum1, "LOCATION_ALERT": randomNum2, "MEORx": randomNum3, "POWER_SUPPLY": randomNum4,"UP_DOWN_CON": randomNum5, "timestamp": randomNum6};
     ws.send(JSON.stringify(jdata));    
